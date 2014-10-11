2372d8615c7659fd2d5e08bbc25afe5e6073f453
Charles Gutjahr <github@charlesgutjahr.com>
2014-10-11 11:36:32 +1100

    Initial checkin Set up a simple, self-contained Spring Boot web application with Thymeleaf templates and the standard Bootstrap framework. I haven't developed any functionality yet, this is just the starting point.

---
+++ src/main/java/com/charlesgutjahr/watpiyn/controller/HomepageController.java
@@ -0,0 +1,14 @@
+package com.charlesgutjahr.watpiyn.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomepageController {
+
+  @RequestMapping("/")
+  public String index() {
+    return "test";
+  }
+
+}
